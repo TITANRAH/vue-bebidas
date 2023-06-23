62546294c0c09afdf0dac8808dd6ec18afbcdc1a
Sergio <sergio.miranda@medismart.cl>
2023-06-22 20:51:09 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from './components/Header.vue';
+
+
+
+</script>
+
+<template>
+<Header/>
+
+ 
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+</script>
+<template>
+    <header>
+        <div class="mx-auto container px-5 py-5">
+
+            <div>
+
+            </div>
+
+            <form 
+            class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+            >
+                <div class="space-y-4">
+                    <label 
+                      class="block text-white uppercase font-extrabold text-lg"
+                      for="ingrediente">Nombre o Ingrediente</label>
+                    <input 
+                      id="ingrediente" 
+                      type="text"
+                      class="p-3 w-full rounded-lg focus:outline-none"
+                      placeholder="Nombre o ingrediente ej. Vodka ,Tequila, etc."
+                      >
+                </div>
+
+                <div class="space-y-4">
+                    <label 
+                      class="block text-white uppercase font-extrabold text-lg"
+                      for="categoria">Nombre o Ingrediente</label>
+                    <select 
+                      id="categoria"                    
+                      class="p-3 w-full rounded-lg focus:outline-none"
+                      >
+
+                      <option value="">-- Seleccione --</option>
+
+                      </select>
+                </div>
+
+                <input 
+                
+                    type="submit"
+                    value="Buscar Recetas"
+                    class="bg-orange-800 hover:bg-orange-900 cursor-pointer text-white font-extrabold w-full p-2 rounded-lg uppercase"
+                    >
+
+            </form>
+
+        </div>
+
+    </header>
+</template>
+
